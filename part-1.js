c6ee09e6d6de11fcbaef5854c140869acdaa4260
Rachel M. Collins <RachelmmCollins@gmail.com>
2023-07-30 17:53:54 -0700

    Generate random coordinates to place a ship, have the amount of ships occupy a coordinate, check the syntax of a guess to be valid or not
    create a battleship game with a 3 by 3 board and two ships and stores guess that are different. Create a function the returns coordinates that fall within a through c and 1 through 3, by starting the return with rows that is one of the letters within the letters array that only includes a b and c. The placeShips function continues to push random coordinates generated from the getRandomCoordinates function until the both ships have a coordinate to occupy

---
+++ part-1.js
@@ -0,0 +1,50 @@
+const readlineSync = require('readline-sync');
+
+class BattleshipsGame {
+    constructor() {
+        this.board = Array(3)
+        .fill(null)
+        .map(() => Array(3)
+        .fill(null));
+
+        this.ships = 2;
+        this.shipCoordinates = [];
+        this.guesses = new Set();
+    }
+    
+    /* 
+    this function will return a string consisting of a letter and a 
+    number between a-b and 1-3 
+    */
+    getRandomCoordinate() {
+        const letters = ["a", "b", "c"];
+        const rows = letters[Math.floor(Math.random() * letters.length)];
+        const colums = Math.floor(Math.random() * 3) + 1;
+        return (rows + colums);
+    }
+
+    /*
+    **this function will place a ship on a coordinate until all ships are placed
+    **by 
+    */
+    placeShips() {
+        for (let i = 0; i < this.ships; i++) {
+            let placement = this.getRandomCoordinate();
+            while (this.shipCoordinates.includes(placement)) {
+              placement = this.getRandomCoordinate();
+            }
+            this.shipCoordinates.push(placement);
+        } 
+    }
+
+    /*
+    ** this function will utilize regex to make sure the guess starts with a 
+    ** letter either a, b, or c and that the guess ends with either 1, 2, or
+    ** 3. 
+    */
+    isValidGuess(guess) {
+        return /^[a-c][1-3]$/i.test(guess);
+    }
+
+}
+const game = new BattleshipsGame();
